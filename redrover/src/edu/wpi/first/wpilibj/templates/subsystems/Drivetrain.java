22dd119739a575de49cb505cea7fb5920028c583
Paul Terrasi <Paul.A.Terrasi@gmail.com>
2014-09-25 19:22:29 -0400

    Fixed error with Drivetrain class

--- redrover/src/edu/wpi/first/wpilibj/templates/subsystems/Drivetrain.java
+++ redrover/src/edu/wpi/first/wpilibj/templates/subsystems/Drivetrain.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+
 /**
  *
  * @author michaelsilver
@@ -16,9 +17,28 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Drivetrain extends PIDSubsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+    
+    static final double kP = 1;
+    static final double kI = 0;
+    static final double kD = 0;
+    static final double kF = 0;
+    static final int PERIOD = 20;
+    
+    public Drivetrain()
+    {
+        super(kP, kI, kD, PERIOD, kF);
+    }
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+
+    protected double returnPIDInput() {
+        return 0;
+    }
+
+    protected void usePIDOutput(double d) {
+        
     }
 }
