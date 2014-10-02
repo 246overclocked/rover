793ebe66bce5f8ff7b8b63161e0a838dbeda8966
michaelsilver <yomichael88@yahoo.com>
2014-10-02 17:57:42 -0400

    SwerveModule and PID constructors (begun)

--- redrover/src/AwfulNameViaPaul/SwerveModule.java
+++ redrover/src/AwfulNameViaPaul/SwerveModule.java
@@ -29,8 +29,31 @@ public class SwerveModule
     PIDController speedPID;
     PIDController anglePID;
     
+    public static final double SPEED_Kp = 0.5;
+    public static final double SPEED_Ki = 0;
+    public static final double SPEED_Kd = 0;
+    public static final double SPEED_Kf = 0;
+    public static final double SPEED_PERIOD = 10;
+    public static final double SPEED_TOLERANCE = 0.25;
+    
+    public static final double ANGLE_Kp = 0.1;
+    public static final double ANGLE_Ki = 0;
+    public static final double ANGLE_Kd = 0;
+    public static final double ANGLE_Kf = 0;
+    public static final double ANGLE_PERIOD = 10;
+    public static final double ANGLE_TOLERANCE = 0.5;
+    
     public SwerveModule(Encoder encoder, AnalogChannel modulePot, SpeedController wheelMotor, SpeedController moduleMotor)
     {
+        this.encoder = encoder;
+        this.modulePot = modulePot;
+        this.wheelMotor = wheelMotor;
+        this.moduleMotor = moduleMotor;
         
+        speedPID = new PIDController(SPEED_Kp, SPEED_Ki, SPEED_Kd, SPEED_Kf, encoder, wheelMotor, SPEED_PERIOD);
+        anglePID = new PIDController(ANGLE_Kp, ANGLE_Ki, ANGLE_Kd, ANGLE_Kf, modulePot, moduleMotor, ANGLE_PERIOD);
+        speedPID.setAbsoluteTolerance(SPEED_TOLERANCE);
+        anglePID.setAbsoluteTolerance(ANGLE_TOLERANCE);
+        anglePID.setContinuous(true);
     }
 }
