b8c18b1437600217167ddbb7979eb585822ed339
michaelsilver <yomichael88@yahoo.com>
2014-10-09 17:33:19 -0400

    swerveModule constructors

--- redrover/src/edu/wpi/first/wpilibj/templates/subsystems/Drivetrain.java
+++ redrover/src/edu/wpi/first/wpilibj/templates/subsystems/Drivetrain.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package edu.wpi.first.wpilibj.templates.subsystems;
+import AwfulNameViaPaul.SwerveModule;
 import edu.wpi.first.wpilibj.templates.RobotMap;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
@@ -18,13 +19,24 @@ public class Drivetrain extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     
+    SwerveModule frontLeftModule;
+    SwerveModule frontRightModule;
+    SwerveModule backLeftModule;
+    SwerveModule backRightModule;
+    
     public Drivetrain()
     {
-        
+        frontLeftModule = new SwerveModule(RobotMap.frontLeftWheelEncoder, RobotMap.frontLeftModulePot, RobotMap.frontLeftWheelMotor, RobotMap.frontLeftModuleMotor);
+        frontRightModule = new SwerveModule(RobotMap.frontRightWheelEncoder, RobotMap.frontRightModulePot, RobotMap.frontRightWheelMotor, RobotMap.frontRightModuleMotor);
+        backLeftModule = new SwerveModule(RobotMap.backLeftWheelEncoder, RobotMap.backLeftModulePot, RobotMap.backLeftWheelMotor, RobotMap.backLeftModuleMotor);
+        backRightModule = new SwerveModule(RobotMap.backRightWheelEncoder, RobotMap.backRightModulePot, RobotMap.backRightWheelMotor, RobotMap.backRightModuleMotor);
     }
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+        
     }
+    
+    
 }
