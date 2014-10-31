db6935717ddf328d924062b0900eed47c86dd57b
michaelsilver <yomichael88@yahoo.com>
2014-10-31 17:22:45 -0400

    extended abstract DrivingCommand with CrabWithTwist, our two-joystick drive scheme

---
+++ redrover/src/edu/wpi/first/wpilibj/templates/commands/CrabWithTwist.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands;
+
+import Libraries.Vector2D;
+
+/**
+ *
+ * @author michaelsilver
+ */
+public class CrabWithTwist extends DrivingCommand{
+
+//    driverLeftJoystick is controlling "twist"
+//    driverRightJoystick is controlling "crab"
+    
+    protected Vector2D getCrabVector() {
+        return new Vector2D(true, oi.driverRightJoystick.getX(), oi.driverRightJoystick.getY());
+    }
+
+    protected double getSpinRate() {
+        return oi.driverLeftJoystick.getX();
+    }
+
+    protected Vector2D getCOR() {
+        return new Vector2D(true, 0, 0);
+    }
+
+    protected void initialize() {
+        
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+        
+    }
+
+    protected void interrupted() {
+        
+    }
+    
+}
