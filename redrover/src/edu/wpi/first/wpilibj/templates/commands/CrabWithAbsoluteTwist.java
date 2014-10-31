0dc8eb162dd743598343d2eb28ab90fa8d9ea866
michaelsilver <yomichael88@yahoo.com>
2014-10-31 19:45:44 -0400

    added bare bones for CrabWithAbsoluteTwist

---
+++ redrover/src/edu/wpi/first/wpilibj/templates/commands/CrabWithAbsoluteTwist.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands;
+
+import Libraries.Vector2D;
+
+/**
+ *
+ * @author michaelsilver
+ */
+public class CrabWithAbsoluteTwist extends CrabWithTwist{
+    // TODO: implement PID. Setpoint is the angle at whih we want robot to point.
+}
