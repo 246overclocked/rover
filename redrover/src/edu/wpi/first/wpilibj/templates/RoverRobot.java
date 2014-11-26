f16147b0842641b25501bee8811481ddab68370b
PaulTerrasi <paul.a.terrasi@gmail.com>
2014-11-25 19:56:58 -0500

    Removed GSON because it was causing problems

--- redrover/src/edu/wpi/first/wpilibj/templates/RoverRobot.java
+++ redrover/src/edu/wpi/first/wpilibj/templates/RoverRobot.java
@@ -7,8 +7,6 @@
 
 package edu.wpi.first.wpilibj.templates;
 
-
-import com.google.gson.Gson;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
@@ -88,7 +86,6 @@ public class RoverRobot extends IterativeRobot implements Runnable {
     
     public void run()
     {
-        Gson gson = new Gson();
         /*
         String[] keys = {
             "time",
